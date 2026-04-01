5d8b5923e67fe90bc03fbb4ce2a5fda692bd950c
2No2Name <2No2Name@web.de>
2026-04-01 19:10:39 +0200

    Switch to 26.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,16 +7,16 @@ plugins {
 
 // Fabric: https://fabricmc.net/develop/
 // Neoforge: https://neoforged.net/
-val MINECRAFT_VERSION by extra { "26.1" } //MUST manually update fabric.mod.json and neoforge.mods.toml
-val NEOFORGE_VERSION by extra { "26.1.0.5-beta" }
-val FABRIC_LOADER_VERSION by extra { "0.18.5" }
-val FABRIC_API_VERSION by extra { "0.144.3+26.1" }
+val MINECRAFT_VERSION by extra { "26.1.1" } //MUST manually update fabric.mod.json and neoforge.mods.toml
+val NEOFORGE_VERSION by extra { "26.1.1.0-beta" }
+val FABRIC_LOADER_VERSION by extra { "0.18.6" }
+val FABRIC_API_VERSION by extra { "0.145.2+26.1.1" }
 
 // This value can be set to null to disable Parchment.
 val PARCHMENT_VERSION by extra { null }
 
 // https://semver.org/
-val MOD_VERSION by extra { "0.22.1" }
+val MOD_VERSION by extra { "0.23.0" }
 
 allprojects {
     apply(plugin = "java")
