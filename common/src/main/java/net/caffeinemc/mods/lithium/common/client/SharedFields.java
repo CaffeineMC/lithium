dfca52c7fee24bddd017f57fa1f36efa5ad8a000
2No2Name <2No2Name@web.de>
2026-04-05 20:58:27 +0200

    Fix crash when experimental client brain mixin is disabled

--- common/src/main/java/net/caffeinemc/mods/lithium/common/client/SharedFields.java
+++ common/src/main/java/net/caffeinemc/mods/lithium/common/client/SharedFields.java
@@ -8,14 +8,21 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class SharedFields {
     public static final AtomicInteger MAXIMUM_BIOME_PARTICLE_CHANCE = new AtomicInteger(Float.floatToIntBits(0.0F)); //Using atomic integer as replacement for atomic float
+
+    //mixin.experimental.client_tick.entity.unused_brain must be enabled for the following fields
     public static final MemorySlot<?> DUMMY_SLOT;
     public static final Brain<?> DUMMY_BRAIN;
 
     static {
-        DUMMY_SLOT = MemorySlot.create();
 
-        var brain = new Brain<>();
-        ((BrainExtended) brain).lithium$pretendAllMemoryTypesRegistered();
-        DUMMY_BRAIN = brain;
+        if (BrainExtended.class.isAssignableFrom(Brain.class)) {
+            DUMMY_SLOT = MemorySlot.create();
+            var brain = new Brain<>();
+            ((BrainExtended) brain).lithium$pretendAllMemoryTypesRegistered();
+            DUMMY_BRAIN = brain;
+        } else {
+            DUMMY_SLOT = null;
+            DUMMY_BRAIN = null;
+        }
     }
 }
