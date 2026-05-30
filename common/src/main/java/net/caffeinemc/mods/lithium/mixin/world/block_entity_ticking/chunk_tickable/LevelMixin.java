c3e2b3d7d9a1ee6f465d7b406eb5621f50c75f53
2No2Name <2No2Name@web.de>
2026-05-31 01:15:23 +0200

    Initialize LongRef to Long.MIN_VALUE to avoid incorrect cache hit at x,z=0,0
    Fixes block entity ticking in lazy loaded chunk with coordinates x,z = 0,0 when first ticking block entity is in that chunk

--- common/src/main/java/net/caffeinemc/mods/lithium/mixin/world/block_entity_ticking/chunk_tickable/LevelMixin.java
+++ common/src/main/java/net/caffeinemc/mods/lithium/mixin/world/block_entity_ticking/chunk_tickable/LevelMixin.java
@@ -7,10 +7,20 @@ import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Level.class)
 public class LevelMixin {
+
+    @Inject(
+            method = "tickBlockEntities",
+            at = @At(value = "HEAD")
+    )
+    private void initPos(CallbackInfo ci, @Share("ShouldTickPos") LocalLongRef lastTickableChunk) {
+        lastTickableChunk.set(Long.MIN_VALUE);
+    }
 
     @Redirect(
             method = "tickBlockEntities",
