eb3b6124767650dee3862b88c833563718c1299e
2No2Name <2No2Name@web.de>
2026-07-10 14:54:07 +0200

    Fix EntityClassGroup missing entries when multiple entity types per class are decided using entity type parameter

--- common/src/main/java/net/caffeinemc/mods/lithium/common/entity/EntityClassGroup.java
+++ common/src/main/java/net/caffeinemc/mods/lithium/common/entity/EntityClassGroup.java
@@ -1,6 +1,6 @@
 package net.caffeinemc.mods.lithium.common.entity;
 
-import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet;
+import it.unimi.dsi.fastutil.objects.Object2ByteOpenHashMap;
 import it.unimi.dsi.fastutil.objects.Reference2ByteOpenHashMap;
 import it.unimi.dsi.fastutil.objects.ReferenceReferenceImmutablePair;
 import net.caffeinemc.mods.lithium.common.reflection.ReflectionUtil;
@@ -53,7 +53,7 @@ public class EntityClassGroup {
 
     private final BiPredicate<Class<?>, Supplier<EntityType<?>>> classAndTypeFitEvaluator;
     private volatile Reference2ByteOpenHashMap<Class<?>> class2GroupContains; // 0: Not contained (decision based on class only), 1: Contained (decision based on class only), 2: Check containedClassAndTypePairs (decision based on entity type)
-    private volatile @Nullable ObjectOpenHashSet<ReferenceReferenceImmutablePair<Class<?>, EntityType<?>>> containedClassAndTypePairs; //only used if decision is based on entity type
+    private volatile @Nullable Object2ByteOpenHashMap<ReferenceReferenceImmutablePair<Class<?>, EntityType<?>>> containedClassAndTypePairs; //only used if decision is based on entity type
 
     public EntityClassGroup(BiPredicate<Class<?>, Supplier<EntityType<?>>> classAndTypeFitEvaluator) {
         this.classAndTypeFitEvaluator = classAndTypeFitEvaluator;
@@ -61,8 +61,9 @@ public class EntityClassGroup {
     }
 
     public void clear() {
-        this.class2GroupContains = new Reference2ByteOpenHashMap<>();
-        this.class2GroupContains.defaultReturnValue(ABSENT_VALUE);
+        Reference2ByteOpenHashMap<Class<?>> newMap = new Reference2ByteOpenHashMap<>();
+        newMap.defaultReturnValue(ABSENT_VALUE);
+        this.class2GroupContains = newMap;
         this.containedClassAndTypePairs = null;
     }
 
@@ -82,8 +83,14 @@ public class EntityClassGroup {
         if (contains == ABSENT_VALUE) {
             return this.testAndAddClass(entityClass, entityType);
         } else {
-            var containedPairs = this.containedClassAndTypePairs;
-            return containedPairs != null && containedPairs.contains(ReferenceReferenceImmutablePair.of(entityClass, entityType));
+            var map = this.containedClassAndTypePairs;
+            if (map != null) {
+                contains = map.getByte(ReferenceReferenceImmutablePair.of(entityClass, entityType));
+                if (contains != ABSENT_VALUE) {
+                    return contains == 1;
+                }
+            }
+            return this.testAndAddClass(entityClass, entityType);
         }
     }
 
@@ -93,40 +100,50 @@ public class EntityClassGroup {
         //it could also be fixed by using an AtomicReference's CAS, but we are writing very rarely (less than 150 times for the total game runtime in vanilla)
         synchronized (this) {
             //test the same condition again after synchronizing, as the collection might have been updated while this thread blocked
-            if (this.class2GroupContains.containsKey(entityClass)) {
-                return this.contains(entityClass, entityType);
+            byte containsInfo = this.class2GroupContains.getByte(entityClass);
+            var pairMap = this.containedClassAndTypePairs;
+            if (containsInfo == 0 || containsInfo == 1) {
+                return containsInfo == 1;
+            } else if (containsInfo == 2 && pairMap != null) {
+                containsInfo = pairMap.getByte(ReferenceReferenceImmutablePair.of(entityClass, entityType));
+                if (containsInfo == 0 || containsInfo == 1) {
+                    return containsInfo == 1;
+                }
             }
 
-            //construct new map instead of updating the old map to avoid thread safety problems
-            //the map is not modified after publication
-            Reference2ByteOpenHashMap<Class<?>> newMap = this.class2GroupContains.clone();
             boolean[] accessedEntityType = new boolean[1];
             Supplier<EntityType<?>> entityTypeSupplier = () -> {
                 accessedEntityType[0] = true;
                 return entityType;
             };
             contains = this.classAndTypeFitEvaluator.test(entityClass, entityTypeSupplier);
-            byte containsInfo = contains ? (byte) 1 : (byte) 0;
+            containsInfo = contains ? (byte) 1 : (byte) 0;
             if (accessedEntityType[0]) {
-                containsInfo = 2; //2: The class group decision is based on both class and type
+                Object2ByteOpenHashMap<ReferenceReferenceImmutablePair<Class<?>, EntityType<?>>> newPairMap = this.containedClassAndTypePairs;
+                newPairMap = newPairMap == null ? new Object2ByteOpenHashMap<>() : newPairMap.clone();
+                newPairMap.defaultReturnValue(ABSENT_VALUE);
+                newPairMap.put(ReferenceReferenceImmutablePair.of(entityClass, entityType), containsInfo);
+                //publish the new set in a volatile field, so that all threads reading after this write can also see all changes to the map done beforehand
+                //since modification on happens in the synchronized block, progress won't be lost
+                this.containedClassAndTypePairs = newPairMap;
 
-                ObjectOpenHashSet<ReferenceReferenceImmutablePair<Class<?>, EntityType<?>>> newPairSet = this.containedClassAndTypePairs;
-                newPairSet = newPairSet == null ? new ObjectOpenHashSet<>() : newPairSet.clone();
-                if (contains) {
-                    newPairSet.add(ReferenceReferenceImmutablePair.of(entityClass, entityType));
-                    //publish the new set in a volatile field, so that all threads reading after this write can also see all changes to the map done beforehand
-                    //since modification on happens in the synchronized block, progress won't be lost
-                    this.containedClassAndTypePairs = newPairSet;
-                }
+                containsInfo = 2; //2: The class group decision is based on both class and type
             }
 
-            byte previousContainsInfo = newMap.put(entityClass, containsInfo);
+            //construct new map instead of updating the old map to avoid thread safety problems
+            //the map is not modified after publication
+            byte previousContainsInfo = this.class2GroupContains.getByte(entityClass);
             if (previousContainsInfo != ABSENT_VALUE && previousContainsInfo != containsInfo) {
                 throw new IllegalStateException("Entity class group class fit evaluator must be a pure function! Class fit for " + entityClass + " changed from " + previousContainsInfo + " to " + containsInfo + " when evaluating for " + entityType + "!");
             }
 
-            //publish the new map in a volatile field, so that all threads reading after this write can also see all changes to the map done beforehand
-            this.class2GroupContains = newMap;
+            if (previousContainsInfo == ABSENT_VALUE) {
+                Reference2ByteOpenHashMap<Class<?>> newMap = this.class2GroupContains.clone();
+                newMap.defaultReturnValue(ABSENT_VALUE);
+                newMap.put(entityClass, containsInfo);
+                //publish the new map in a volatile field, so that all threads reading after this write can also see all changes to the map done beforehand
+                this.class2GroupContains = newMap;
+            }
         }
         return contains;
     }
