62704c6a7d4c43a5fd145b327079ed2fa13fa4f1
2No2Name <2No2Name@web.de>
2026-09-26 14:34:37 +0200

    Cleanup ice spreading optimization
    Attempts to fix https://github.com/CaffeineMC/lithium/issues/777

--- common/src/main/java/net/caffeinemc/mods/lithium/mixin/world/chunk_ticking/spread_ice/BiomeMixin.java
+++ common/src/main/java/net/caffeinemc/mods/lithium/mixin/world/chunk_ticking/spread_ice/BiomeMixin.java
@@ -1,12 +1,13 @@
 package net.caffeinemc.mods.lithium.mixin.world.chunk_ticking.spread_ice;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.biome.Biome;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.FluidState;
-import net.minecraft.world.level.material.Fluids;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -14,36 +15,14 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 @Mixin(Biome.class)
 public class BiomeMixin {
 
-    @Redirect(
+    @WrapOperation(
             method = "shouldFreeze(Lnet/minecraft/world/level/LevelReader;Lnet/minecraft/core/BlockPos;Z)Z",
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/world/level/LevelReader;getFluidState(Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/material/FluidState;"
             )
     )
-    private FluidState getNull(LevelReader instance, BlockPos blockPos) {
-        return null;
-    }
-
-    @Redirect(
-            method = "shouldFreeze(Lnet/minecraft/world/level/LevelReader;Lnet/minecraft/core/BlockPos;Z)Z",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/world/level/material/FluidState;is(Ljava/lang/Object;)Z"
-            )
-    )
-    private boolean skipFluidCheck(FluidState instance, Object o) {
-        return true;
-    }
-
-    @Redirect(
-            method = "shouldFreeze(Lnet/minecraft/world/level/LevelReader;Lnet/minecraft/core/BlockPos;Z)Z",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/world/level/block/state/BlockState;getBlock()Lnet/minecraft/world/level/block/Block;"
-            )
-    )
-    private Block fluidCheckAndGetBlock(BlockState blockState) {
-        return blockState.getFluidState().is(Fluids.WATER) ? blockState.getBlock() : null;
+    private FluidState getFluidStateFromBlockState(LevelReader instance, BlockPos blockPos, Operation<FluidState> original, @Local(name = "blockState") BlockState blockState) {
+        return blockState != null ? blockState.getFluidState() : original.call(instance, blockPos);
     }
 }
